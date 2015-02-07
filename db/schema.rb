12ebc1b0d4ce660e8eb5f46bfc034934aa8f3c19
yutakakinjyo <yutakakinjyo@gmail.com>
2015-02-07 18:24:58 +0900

    rake db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150207091600) do
+ActiveRecord::Schema.define(version: 20150207092342) do
+
+  create_table "attendances", force: true do |t|
+    t.integer  "course_id"
+    t.integer  "user_id"
+    t.date     "date"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "courses", force: true do |t|
     t.integer  "user_id"
