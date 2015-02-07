3e6cca1caf447fbb8498977c13ba674ed6500799
yutakakinjyo <yutakakinjyo@gmail.com>
2015-02-07 18:13:32 +0900

    rails g migration AddNameToUsers; rake db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150207090703) do
+ActiveRecord::Schema.define(version: 20150207091213) do
 
   create_table "roles", force: true do |t|
     t.string   "name"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 20150207090703) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "role_id"
+    t.string   "name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
