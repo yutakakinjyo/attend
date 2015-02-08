9fa3417425ee8a0e3faf297a9591ece564b570fc
yutakakinjyo <yutakakinjyo@gmail.com>
2015-02-08 11:21:20 +0900

    add association to models

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,2 +1,4 @@
 class Course < ActiveRecord::Base
+  has_many :course_details
+  has_many :attendances
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  has_many :course_details
+  has_many :attendances
+  belongs_to :role
+
 end
