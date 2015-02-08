c695c792453d37bcc23d49417547dc19f40a182f
yutakakinjyo <yutakakinjyo@gmail.com>
2015-02-08 11:27:52 +0900

    add though option

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  has_many :courses, through: :course_details
   has_many :course_details
   has_many :attendances
   belongs_to :role
