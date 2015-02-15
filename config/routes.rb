26faa6914ae936c4cb0d96ed124b8cfb4c7299a6
yutakakinjyo <yutakakinjyo@gmail.com>
2015-02-15 12:19:35 +0900

    create user profile page and add link to edit user registration path

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   resources :course_details, only: [:new, :create, :destroy]
 
   devise_for :users
+  resources :users, only: [:show]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
